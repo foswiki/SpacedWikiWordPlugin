972de05488d22b468478f31a2f60bdc8e93928aa
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2008-12-14 17:49:56 +0000

    Item27: Fix svn properties on all .pm, .tmpl and .txt files
    git-svn-id: http://svn.foswiki.org/trunk/SpacedWikiWordPlugin@1340 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Plugins/SpacedWikiWordPlugin.pm
+++ lib/Foswiki/Plugins/SpacedWikiWordPlugin.pm
@@ -24,10 +24,10 @@ use vars qw(
   $web $topic $user $installWeb $VERSION $RELEASE $debug %dontSpaceSet $spaceOutWikiWordLinks $spaceOutUnderscoreLinks $removeAnchorDashes
 );
 
-# This should always be $Rev: 13634 $ so that Foswiki can determine the checked-in
+# This should always be $Rev$ so that Foswiki can determine the checked-in
 # status of the plugin. It is used by the build automation tools, so
 # you should leave it alone.
-$VERSION = '$Rev: 13634 $';
+$VERSION = '$Rev$';
 
 # This is a free-form string you can use to "name" your own plugin version.
 # It is *not* used by the build automation tools, but is reported as part
